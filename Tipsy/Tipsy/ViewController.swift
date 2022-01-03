97d13c5b7142a932bc3830adb5d602a4d016cd1f
loopbackseal <young961027@gmail.com>
2022-01-03 17:00:30 +0900

    Initial Set up

---
+++ Tipsy/Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Young Soo Hwang on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
